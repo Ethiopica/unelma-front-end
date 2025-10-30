b600e1fa7d47e928ac9a89c3ae5562fce4e60932
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-10-30 02:10:40 +0200

    Add routing and pages (Home, About, Products, Contact, NotFound)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./pages/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import Products from "./pages/Products.jsx";
+import Blog from "./pages/Blog.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
 function App() {
   return (
     <>
-      <Layout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
